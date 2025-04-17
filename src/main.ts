448e6b2a119b12e69085ec299af45a0a8f1d7954
Marie Turco <marieturco.07@gmail.com>
2025-04-17 12:15:44 +0200

    Ajout Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 // utilisé poour initialiser une instance de l'appli
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 // Fonction asynchrone bootstrap qui démarre l'appli
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 🔹 Configuration Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Météo')
+    .setDescription('Une API qui fournit la météo pour une ville donnée')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Swagger dispo sur /api
+
   // démarrage du serveur, écoute sur port 3000
   await app.listen(process.env.PORT ?? 3000);
 }
